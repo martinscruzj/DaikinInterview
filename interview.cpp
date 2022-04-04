6b7fbe3f5a49d0bbb38ab9d59efd947548fbc082
Martin <101901076+martinscruzj@users.noreply.github.com>
2022-04-04 08:55:35 -0700

    Add factorial function

--- interview.cpp
+++ interview.cpp
@@ -2,7 +2,25 @@
 
 using namespace std;
 
+int factorial(int n) {
+    int result = 0;
+    if (n > 0) {
+        result = n * factorial(n - 1);
+    } else if (n == 0) {
+        result = 1;
+    } else {
+        cout << "Error can't get negative input factorial" << endl;
+    }
+    return result;
+}
+
 int main(int argc, char** argv) {
-    cout << "Hello" << endl;
+    int f = 0;
+    f = factorial(0);
+    cout << "0 factorial is " << f << endl;
+    f = factorial(1);
+    cout << "1 factorial is " << f << endl;
+    f = factorial(4);
+    cout << "4 factorial is " << f << endl;
     return 0;
 }
